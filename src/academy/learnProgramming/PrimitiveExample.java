4badb21b45e69ab7b24410443232b43de6c54a12
venu2804 <venu2804@gmail.com>
2018-10-06 19:44:24 +0100

    First Commit

---
+++ src/academy/learnProgramming/PrimitiveExample.java
@@ -0,0 +1,36 @@
+package academy.learnProgramming;
+
+public class PrimitiveExample {
+
+    public static void main(String[] args) {
+
+        long max = 32145_678_989L;
+
+        long n = 2;
+
+        float mynumber = 2.56f;
+
+        //double
+       /*
+       double before = 10_.25;
+        double after = 10._25;
+        double last = 10.25_;
+        double first = _10.25;
+        */
+
+        char ch = 'a';
+        char uniChar = '\u03A9';
+        char romanChar = '\u0216';
+        System.out.println(uniChar);
+        System.out.println(romanChar);
+
+        boolean myBoolean = true;
+        boolean myFalseBoolean = false;
+        boolean true1 = true;
+
+
+
+
+
+    }
+}
